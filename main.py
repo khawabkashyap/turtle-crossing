8813bf5baebcb9bb9f5533e5110bbfc4042fdefb
khwabkashyap <khawabkashyap@hotmail.com>
2023-09-29 14:39:59 +0530

    final commit

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
+import random
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+cars = CarManager()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(player.move_up, 'Up')
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cars.create_new_car()
+    cars.move()
+
+    for car in cars.all_cars:
+        if player.distance(car) < 25:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if player.ycor() > 280:
+        scoreboard.increase_level()
+        cars.increase_car_speed()
+        player.level_up()
+
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,25 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.penup()
+        self.goto(x=-235, y=255)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
+
